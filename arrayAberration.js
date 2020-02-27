24232b749f1f7c337eee6aeedc0f1e1ec769f924
jimmy.saavedra@beslogic.com <jimmy.saavedra@beslogic.com>
2020-02-27 11:48:12 -0500

    Commit subject - Comments
    Commit description - Updating comments

--- arrayAberration.js
+++ arrayAberration.js
@@ -1,19 +1,19 @@
-//JavaScript array aberration 
+// JavaScript array aberration 
 var arr = [11, 22, 33, 44];
 arr.pro1 = '55';
 arr.pro2 = '66';
 
-
+// Using in
 for (i in arr) {
   console.log(i);
   console.log(arr[i]);
 }
 
-
+// Using of
 for (i of arr)
   console.log(i);
 
-//This is the most secure option
+// This is the most secure option
 arr.forEach(i => {
   console.log(i)
 });
